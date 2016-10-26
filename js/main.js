324d46eba27cb93db872564762f155e422cf1e6a
AlejandraEscobar3G <ale.fesico@gmail.com>
2016-10-26 13:34:40 -0500

    Version 0.0.2 sin mensaje clickeable

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ var boton = document.getElementById("AgregarLista");
 boton.addEventListener("click", agregarLista);
 
 function agregarLista(){
+	var diiv = document.getElementById("lista");
 	var pre = document.getElementById("contenedor");
-	
+
 	// formulario
 	var formulario = document.createElement("DIV");
 	formulario.classList.add("form-group");
@@ -17,6 +18,17 @@ function agregarLista(){
 	bton.appendChild(texto);
 	formulario.appendChild(input);
 	formulario.appendChild(bton);
-	pre.appendChild(formulario);
+	diiv.appendChild(formulario);
 
+	//Creando eventos en GUARDAR
+	bton.onclick = function(){
+		var txt = input.value;
+		var titulo = document.createTextNode(txt);
+
+		var h3 = document.createElement("h3");
+
+		h3.appendChild(titulo);
+		pre.appendChild(h3);
+		diiv.removeChild(formulario);
+	}
 }
