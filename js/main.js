997c92914a80546a4b182a59085e339783887141
AlejandraEscobar3G <ale.fesico@gmail.com>
2016-10-27 16:07:48 -0500

    Version 0.0.3

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@ var boton = document.getElementById("AgregarLista");
 boton.addEventListener("click", agregarLista);
 
 function agregarLista(){
-	var diiv = document.getElementById("lista");
 	var pre = document.getElementById("contenedor");
-
+	pre.removeChild(boton);
 	// formulario
 	var formulario = document.createElement("DIV");
 	formulario.classList.add("form-group");
@@ -18,18 +17,27 @@ function agregarLista(){
 	bton.appendChild(texto);
 	formulario.appendChild(input);
 	formulario.appendChild(bton);
-	diiv.appendChild(formulario);
+	pre.appendChild(formulario);
 
-	//Creando eventos en GUARDAR
+	//Creando eventos en GUARDAR (0.0.2)
 	bton.onclick = function(){
 		var txt = input.value;
 		var titulo = document.createTextNode(txt);
 
-		var h3 = document.createElement("h3");
+		pre.removeChild(formulario);
 
-		h3.appendChild(titulo);
-		pre.appendChild(h3);
-		diiv.removeChild(formulario);
-		
+		var tarjeta = document.createElement("div");
+		var h2 = document.createElement("h2");
+		var annadir = document.createTextNode("Añadir tarjeta");
+		var lista = document.createElement("button");
+
+		lista.classList.add("btn", "mt-2");
+
+		h2.appendChild(titulo);
+		lista.appendChild(annadir);
+		tarjeta.appendChild(h2);
+		tarjeta.appendChild(lista);
+		pre.appendChild(tarjeta);
+		pre.appendChild(boton);
 	}
 }
